cc05751e07ee67027d293ca15a72e96de81da8ee
Kuo-Chin Huang <panpanhuang332@gmail.com>
2026-07-01 15:54:43 +0800

    Expand survey research guidance

---
+++ _next/static/Oc4V7BgEucG_VRrRS6mKP/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/research_self_study//_next/:path+",
+        "destination": "/research_self_study/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
